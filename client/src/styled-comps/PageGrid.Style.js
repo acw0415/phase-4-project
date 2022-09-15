3db91f45f27534b1f0abd257701122331131f0ab
Austin <acw0415@gmail.com>
2022-09-15 12:47:56 -0400

    styling

---
+++ client/src/styled-comps/PageGrid.Style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const CenterHome = styled.div`
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export default CenterHome
---
+++ client/src/styled-comps/TitleStyle.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+import theme from "./theme"
+
+const TitleStyle = styled.h1`
+
+font-family: 'Overlock', cursive;
+background-color: ${theme.buttonColor};
+color: white;
+border: none;
+padding: 5px 10px;
+border-radius: 5px;
+margin: 10px;
+
+
+`
+
+export default TitleStyle
